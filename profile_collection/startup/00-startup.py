16c282a96fbb8704f77ae51cbdcd92569b44ec83
CSX-1 Operator <xf23id1@xf23id-ws3.cs.nsls2.local>
2017-08-17 16:15:52 -0400

    WIP upgrade

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -2,40 +2,29 @@
 from ophyd import setup_ophyd
 setup_ophyd()
 
-from metadatastore.mds import MDS
-# from metadataclient.mds import MDS
+# Set up a RunEngine and use metadata backed by a sqlite file.
+from bluesky import RunEngine
+from bluesky.utils import get_history
+RE = RunEngine(get_history())
+
+# Set up a Broker.
 from databroker import Broker
-from databroker.core import register_builtin_handlers
-from filestore.fs import FileStore
-
-# pull from /etc/metadatastore/connection.yaml
-mds = MDS({'host': 'xf23id-broker', 'database': 'datastore2',
-           'port': 27017, 'timezone': 'US/Eastern'}, auth=False)
-# mds = MDS({'host': CA, 'port': 7770})
-
-# pull configuration from /etc/filestore/connection.yaml
-db = Broker(mds, FileStore({'host': 'xf23id-broker',
-                            'database': 'filestore',
-                            'port': 27017}))
-
-register_builtin_handlers(db.event_sources[0].fs)
-
-get_events = db.get_events
-get_images = db.get_images
-get_table = db.get_table
-get_fields = db.get_fields
-restream = db.restream
-process = db.process
+db = Broker.named('csx')
 
 # Subscribe metadatastore to documents.
 # If this is removed, data is not saved to metadatastore.
-from bluesky.global_state import gs
-gs.RE.subscribe_lossless('all', db.insert)
+RE.subscribe(db.insert)
+
+# Set up the BestEffortCallback.
+from bluesky.callbacks.best_effort import BestEffortCallback
+bec = BestEffortCallback()
+RE.subscribe(bec)
+peaks = bec.peaks  # just as alias for less typing
 
 # At the end of every run, verify that files were saved and
 # print a confirmation message.
 from bluesky.callbacks.broker import verify_files_saved
-# gs.RE.subscribe('stop', post_run(verify_files_saved))
+# RE.subscribe(post_run(verify_files_saved), 'stop')
 
 # Import matplotlib and put it in interactive mode.
 import matplotlib.pyplot as plt
@@ -49,15 +38,11 @@ install_qt_kicker()
 # RE.md['beamline_id'] = 'YOUR_BEAMLINE_HERE'
 
 # convenience imports
-from ophyd.commands import *
 from bluesky.callbacks import *
-from bluesky.spec_api import *
-from bluesky.plan_tools import print_summary
-from bluesky.global_state import gs, abort, stop, resume
-from time import sleep
+from bluesky.callbacks.broker import *
+from bluesky.simulators import *
+from bluesky.plans import *
 import numpy as np
-
-RE = gs.RE  # convenience alias
 
 # Uncomment the following lines to turn on verbose messages for
 # debugging.
@@ -65,24 +50,12 @@ RE = gs.RE  # convenience alias
 # ophyd.logger.setLevel(logging.DEBUG)
 # logging.basicConfig(level=logging.DEBUG)
 
-from bluesky.plans import *
-from bluesky.callbacks.broker import *
-from bluesky.callbacks.scientific import plot_peak_stats
-
-
-# Set up default metadata.
-# from historydict import HistoryDict
-# gs.RE.md = HistoryDict(filename)
-gs.RE.md['group'] = ''
-gs.RE.md['config'] = {}
-gs.RE.md['beamline_id'] = 'CSX-1'
-
 # Add a callback that prints scan IDs at the start of each scan.
 def print_scan_ids(name, start_doc):
     print("Transient Scan ID: {0} @ {1}".format(start_doc['scan_id'],time.strftime("%Y/%m/%d %H:%M:%S")))
     print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
 
-gs.RE.subscribe('start', print_scan_ids)
+RE.subscribe(print_scan_ids, 'start')
 
 
-asc = ascan  # alias
+asc = scan  # alias
--- profile_collection/startup/99-settings.py
+++ profile_collection/startup/99-settings.py
@@ -1,42 +1,40 @@
-#gs.COUNT_TIME = 1  # seconds
+from bluesky import SupplementalData
+sd = SupplementalData()
+RE.preprocessors.append(sd)
 
-#
-## These detectors will be read by the scans defined
-## in 98-quick-scans.py, such as lup.
-gs.DETS = [theta, delta, gamma,
-           sx, say, saz, cryoangle,
-           sy, sz,
-           temp, uw_temp,
-           pgm_en,
-           epu1, epu2,
-           slt1, slt2, slt3,
-           m1a, m3a,
-           mono_tempa, mono_tempb,  grt1_temp, grt2_temp]
+# interactive use
+sd.monitors = []  # a list of signals to monitor concurrently
+sd.flyers = []  # a list of "flyable" devices
+# a list of devices to read at start and end
+sd.baseline = [theta, delta, gamma,
+               sx, say, saz, cryoangle,
+               sy, sz,
+               temp, uw_temp,
+               pgm_en,
+               epu1, epu2,
+               slt1, slt2, slt3,
+               m1a, m3a,
+               mono_tempa, mono_tempb,  grt1_temp, grt2_temp]
 
-## After the new implementation in Ophyd/Bluesky
-theta.user_readback.name = 'theta'
-delta.user_readback.name = 'delta'
-gamma.user_readback.name = 'gamma'
+dets = [sclr]
 
 pgm_en.readback.name = 'energy'
 
 sclr.names.read_attrs=['name1','name2','name3','name4','name5','name6']
 sclr.channels.read_attrs=['chan1','chan2','chan3','chan4','chan5','chan6']
+sclr.hints = {'fields': ['sclr_ch2', 'sclr_ch3', 'sclr_ch6']}
 
 
 
 #gs.MASTER_DET = sclr
 #gs.MASTER_DET_FIELD = 'sclr_chan2'
 #
-## These are used by th2th
-gs.TH_MOTOR = theta
-gs.TTH_MOTOR = delta  # an approximation...
 #
 #gs.TABLE_COLS = ['sclr_chan4']
 #gs.PLOT_Y = 'sclr_chan4'
 
-gs.DETS.append(ring_curr)
-gs.DETS.append(nanop)
+#gs.DETS.append(ring_curr)
+#gs.DETS.append(nanop)
 #gs.DETS.append(sm_i)
 #gs.DETS.append(sm_v)
 #gs.DETS.append(sm_r)
@@ -47,10 +45,6 @@ gs.DETS.append(nanop)
 #gs.DETS.append(npty)
 #gs.DETS.append(nptz)
 
-gs.DETS.append(diag6_monitor)
-
-gs.COUNT_TIME = 1
-
 #gs.DETS.append(dif_beam_cam)
 #gs.TABLE_COLS =['dif_beam_cam_stats_total3']
 #gs.PLOT_Y='dif_beam_cam_stats_total3'
@@ -60,6 +54,7 @@ gs.COUNT_TIME = 1
 #gs.DETS.append(fccd)
 #fccd.acquire_period = 1
 #fccd.num_images = 5
+
 
 #gs.TABLE_COLS = ['fccd_stats_total1']
 #gs.PLOT_Y = 'fccd_stats_total1'
@@ -74,93 +69,3 @@ gs.COUNT_TIME = 1
 def relabel_fig(fig, new_label):
     fig.set_label(new_label)
     fig.canvas.manager.set_window_title(fig.get_label())
-
-
-
-
-import os
-from datetime import datetime
-
-def write_spec_header(path, doc):
-    # write a new spec file header!
-    #F /home/xf11id/specfiles/test.spec
-    #E 1449179338.3418093
-    #D 2015-12-03 16:48:58.341809
-    #C xf11id  User = xf11id
-    #O [list of all motors, 10 per line]
-    session_manager = get_session_manager()
-    pos = session_manager.get_positioners()
-    spec_header = [
-        '#F %s' % path,
-        '#E %s' % int(doc['time']),
-        # time might need to be formatted specifically
-        '#D %s' % datetime.fromtimestamp(doc['time']),
-        '#C %s  User = %s' % (doc['owner'], doc['owner']),
-        'O0 {}'.format(' '.join(sorted(list(pos.keys()))))
-    ]
-    with open(path, 'w') as f:
-        f.write('\n'.join(spec_header))
-    return spec_header
-
-class LiveSpecFile(CallbackBase):
-    def start(self, doc):
-        if not os.path.exists(gs.specpath):
-            spec_header = write_spec_header(gs.specpath, doc)
-        #else:
-        #    spec_header = get_spec_header()
-        last_command = list(
-            get_ipython().history_manager.get_range())[-1][2]
-        last_command = last_command.replace('(', ' ')
-        last_command = last_command.replace(')', ' ')
-        last_command = last_command.replace(',', ' ')
-        self.acquisition_time = gs.COUNT_TIME
-        # write a blank line between scans
-        with open(gs.specpath, 'a') as f:
-            f.write('\n\n')
-        # write the new scan entry
-        with open(gs.specpath, 'a') as f:
-            f.write('#S %s %s\n' % (doc['scan_id'], last_command))
-            f.write('#D %s\n' % datetime.fromtimestamp(doc['time']))
-            f.write('#T %s (Seconds)\n' % self.acquisition_time)
-        # write the motor positions
-        session_manager = get_session_manager()
-        pos = session_manager.get_positioners()
-        positions = [str(v.position) for k, v in sorted(pos.items())]
-        with open(gs.specpath, 'a') as f:
-            f.write('#P0 {0}\n'.format(' '.join(positions)))
-        # writing the list of motor names and their current values in
-        # a more human readable way. Apparently "#M" is not a used key in
-        # spec. Fingers crossed....
-        with open(gs.specpath, 'a') as f:
-            for idx, (name, positioner) in enumerate(sorted(pos.items())):
-                f.write('#M%s %s %s\n' % (idx, name, str(positioner.position)))
-        print("RunStart document received in LiveSpecFile")
-        #raise
-        self.motorname = last_command.split(' ')[1]
-
-    def descriptor(self, doc):
-        print('descriptor received')
-        keys = sorted(list(doc['data_keys'].keys()))
-        keys.remove(self.motorname)
-        keys.insert(0, 'Seconds')
-        keys.insert(0, 'Epoch')
-        keys.insert(0, self.motorname)
-        with open(gs.specpath, 'a') as f:
-            f.write('#N %s\n' % len(keys))
-            f.write('#L {0}    {1}\n'.format(keys[0],
-                                             '  '.join(keys[1:])))
-
-    def event(self, doc):
-        print('event received')
-        t = int(doc['time'])
-        vals = [v for k, v in sorted(doc['data'].items()) if k != self.motorname]
-        vals.insert(0, self.acquisition_time)
-        vals.insert(0, t)
-        vals.insert(0, doc['data'][self.motorname])
-        vals = [str(v) for v in vals]
-        with open(gs.specpath, 'a') as f:
-            f.write('{0}  {1}\n'.format(vals[0], ' '.join(vals[1:])))
-
-gs.specpath = os.path.expanduser('~/specfiles/test.spec')
-live_specfile_callback = LiveSpecFile()
-#gs.RE.subscribe('all', live_specfile_callback)
