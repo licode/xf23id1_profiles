a332831cdc145d36b36ee95aa3e7744b7d376fc4
CSX-1 Operator <xf23id1@xf23id-ws3.cs.nsls2.local>
2017-08-22 13:41:16 -0400

    Use threading to make olog cb more fault tolerant.

--- profile_collection/startup/01-olog-integration.py
+++ profile_collection/startup/01-olog-integration.py
@@ -1,6 +1,9 @@
+from bluesky.callbacks.olog import logbook_cb_factory
 from functools import partial
 from pyOlog import SimpleOlogClient
-from bluesky.callbacks.olog import logbook_cb_factory
+import queue
+import threading
+from warnings import warn
 
 # Set up the logbook. This configures bluesky's summaries of
 # data acquisition (scan type, ID, etc.).
@@ -16,11 +19,25 @@ logbook = simple_olog_client
 
 cb = logbook_cb_factory(configured_logbook_func)
 
-def fault_tolerant_olog(name, doc):
-    try:
-        cb(name, doc)
-    except Exception as exc:
-        print('olog error ignored:', exc)
 
-# RE.subscribe('start', fault_tolerant_olog)  ## olog problem -> uncomment when it will
-# stop hunging..
+
+def submit_to_olog(queue, cb):
+    while True:
+        name, doc = queue.get()  # waits until document is available
+        try:
+            cb(name, doc)
+        except Exception as exc:
+            warn('This olog is giving errors. This will not be logged.'
+                 'Error:' + str(exc))
+
+olog_queue = queue.Queue(maxsize=100)
+olog_thread = threading.Thread(target=submit_to_olog, args=(olog_queue, cb), daemon=True)
+olog_thread.start()
+
+def send_to_olog_queue(name, doc):
+    try:
+        olog_queue.put((name, doc), block=False)
+    except queue.Full:
+        warn('The olog queue is full. This will not be logged.')
+
+RE.subscribe(send_to_olog_queue, 'start')
