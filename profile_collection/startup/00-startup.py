06f594852cb1ef62d20b2f84fa7f8ca87d3f5153
Stuart B. Wilkins <swilkins@bnl.gov>
2017-09-26 10:11:04 -0400

    WIP: Before nuking, checkin

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -71,10 +71,10 @@ from pyOlog.ophyd_tools import *
 
 # Add a callback that prints scan IDs at the start of each scan.
 def print_scan_ids(name, start_doc):
-    print("Transient Scan ID: {0} @ {1}".format(start_doc['scan_id'],time.strftime("%Y/%m/%d %H:%M:%S")))
+    print("Transient Scan ID: {0} @ {1}".format(start_doc['scan_id'],
+                                                time.strftime("%Y/%m/%d %H:%M:%S")))
     print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
 
 RE.subscribe(print_scan_ids, 'start')
-
 
 asc = scan  # alias
