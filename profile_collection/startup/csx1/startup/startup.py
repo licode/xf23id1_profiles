2b7917e0d44bd352ed988649634565f144251445
Andi Barbour <abarbour@bnl.gov>
2018-01-11 18:34:34 -0500

    MNT: BEC now does scan start tag line

--- profile_collection/startup/csx1/startup/startup.py
+++ profile_collection/startup/csx1/startup/startup.py
@@ -65,13 +65,5 @@ import numpy as np
 # ophyd.logger.setLevel(logging.DEBUG)
 # logging.basicConfig(level=logging.DEBUG)
 
-# Add a callback that prints scan IDs at the start of each scan.
-def print_scan_ids(name, start_doc):
-    print("Transient Scan ID: {0} @ {1}".format(start_doc['scan_id'],time.strftime("%Y/%m/%d %H:%M:%S")))
-    print("Persistent Unique Scan ID: '{0}'".format(start_doc['uid']))
-
-RE.subscribe(print_scan_ids, 'start')
-
-
 asc = scan  # alias
 rsc = relative_scan # alias
